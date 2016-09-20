99c49367d62abc38c02b7557015a64cd3bb27015
Ihsancom <ihsan1099@hotmail.com>
2016-09-20 14:43:44 +0700

    test

--- app/src/main/java/com/example/ihsan/sutfriend/SignUpActivity.java
+++ app/src/main/java/com/example/ihsan/sutfriend/SignUpActivity.java
@@ -3,11 +3,11 @@ package com.example.ihsan.sutfriend;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ihsan extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ihsan);
+        setContentView(R.layout.activity_sign_up);
     }
-}   // ihsan class นี่คือ คลาสหลัก
+}
