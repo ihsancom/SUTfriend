3690b4ba75673c36f4d8e1807c2e367d8d9ed16e
Ihsancom <ihsan1099@hotmail.com>
2016-09-20 13:20:37 +0700

    Start App

---
+++ app/src/main/java/com/example/ihsan/sutfriend/ihsan.java
@@ -0,0 +1,13 @@
+package com.example.ihsan.sutfriend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ihsan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ihsan);
+    }
+}
